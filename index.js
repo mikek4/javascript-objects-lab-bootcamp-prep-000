86e64472e66db4f4469881d002fc44d9aa843d41
Michael Katz <mike.katz@outlook.com>
2017-10-21 19:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ function destructivelyUpdateObjectWithKeyAndValue (recipes, key, value) {
 recipes[key] = value;
 return recipes
 }
+
+function deleteFromObjectByKey (recipes, key){
+  var recipes = {key}
+  var newRecipes = Object.assign({}, recipes)
+
+  delete newRecipes.key
+  return newRecipes
+}
